288d420eaa4833aef8d147072f232c0896b7395e
jhs1v1 <67949783+jhs1v1@users.noreply.github.com>
2021-06-14 23:59:16 +0900

    0614 Coding

---
+++ ConsoleApp1/12954_pr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution_12954
+{
+    public long[] solution(int x, int n)
+    {
+        long[] answer = new long[n];
+        long a = x;
+
+        for(int i = 0; i < n; i++)
+        {
+            answer[i] = a + i * a;
+        }
+
+        return answer;
+    }
+}
---
+++ ConsoleApp1/12969_pr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Example_12969
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        for(int i = 0; i < b; i++)
+        {
+            for(int j = 0; j < a; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
