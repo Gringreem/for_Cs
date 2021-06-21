4563fa766a9176e2bc4d71d32577c439cba7e7cf
jhs1v1 <67949783+jhs1v1@users.noreply.github.com>
2021-06-21 14:48:18 +0900

    0621 Coding

---
+++ ConsoleApp1/12981_pr.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution_12981
+{
+    public int[] solution(int n, string[] words)
+    {
+        int[] answer = {0,0};
+        bool check = false;
+        int who = 0;
+        int when = 1;
+
+        List<string> list = new List<string>();
+        list.Add(words[0]);
+        who++;
+
+        for(int i = 1; i < words.Length; ++i)
+        {
+            ++who;
+
+            if(who > n)
+            {
+                when++;
+                who -= n;
+            }
+
+            char last = words[i - 1].Last();
+            char first = words[i].First();
+
+            if(list.Contains(words[i]) || char.Equals(last, first) == false)
+            {
+                check = true;
+                break;
+            }
+
+            list.Add(words[i]);
+        }
+
+        if (check == false)
+        {
+            who = 0;
+            when = 0;
+        }
+
+        answer[0] = who;
+        answer[1] = when;
+
+        return answer;
+    }
+}
---
+++ ConsoleApp1/42883_pr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution_42883
+{
+    public string solution(string number, int k)
+    {
+        string answer = "";
+        int size = number.Length - k;
+        List<int> numList = new List<int>(size);
+        int count = 0;
+
+        for(int i = 0; i < size; i++)
+        {
+            char maxC = number[count];
+            int maxI = count;
+            for(int j = count; j <= k + i; j++)
+            {
+                if (maxC < number[j])
+                {
+                    maxC = number[j];
+                    maxI = j;
+                }
+            }
+            count = maxI + 1;
+            numList.Add((int)Char.GetNumericValue(maxC));
+        }
+        answer = string.Join("",numList);
+        return answer;
+
+    }
+}
