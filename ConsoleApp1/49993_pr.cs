bea06830a254d3d66fa31c16f49b487019554ffc
jhs1v1 <67949783+jhs1v1@users.noreply.github.com>
2021-06-23 15:12:39 +0900

    0623 Coding

---
+++ ConsoleApp1/49993_pr.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution_49993
+{
+    public int solution(string skill, string[] skill_trees)
+    {
+        int answer = 0;
+        
+        foreach(var str in skill_trees)
+        {
+            bool right = true;
+            List<char> str_list = str.ToCharArray().ToList();
+
+            for(int i = 0; i < skill.Length - 1; ++i)
+            {
+                int prev = str_list.FindIndex(x => x == skill[i]);
+                int next = str_list.FindIndex(x => x == skill[i+1]);
+
+                if((prev>next && next>=0) || (prev<0 && next >= 0))
+                {
+                    right = false;
+                    break;
+                }
+            }
+            
+            if (right)
+            {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ ConsoleApp1/76502_pr.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = 0;
+        bool check = true;
+        char[] str = s.ToArray();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            string new_str = "";
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (i + j < 6)
+                {
+                    new_str += str[i + j];
+                }
+                else
+                {
+                    new_str += str[i + j - 6];
+                }
+            }
+
+            List<char> stack = new List<char>();
+
+            for (int j = 0; j < new_str.Length; j++)
+            {
+                int top = -1;
+                if (new_str[j] == '[' || new_str[j] == '{' || new_str[j] == '(')
+                {
+                    stack.Add(new_str[j]);
+                    top++;
+                }
+                else if (top < 0)
+                {
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    stack.Remove(stack[top--]);
+                }
+            }
+            if(check == true)
+            {
+                answer++;
+            }
+        }
+
+
+
+        return answer;
+    }
+}
